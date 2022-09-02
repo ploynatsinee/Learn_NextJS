03d6cf1e4d1c1e6d0570807ff9c6cdf84ee272cd
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-09-02 14:56:15 +0700

    fix text

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
+
 
 export default function Home() {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <h1>Hello World Ka</h1>
     </div>
   )
 }
